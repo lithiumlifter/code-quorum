93f376332d8a1b2f80ad2b510a71100c89722f90
rizkycavendio <mrc.dio2001@gmail.com>
2024-05-01 16:18:29 +0700

    add main section home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,10 @@
         <title>Code Quorum</title>
 
         @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+         <!-- Favicon-->
+         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+         <!-- Font Awesome icons (free version)-->
+         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="antialiased">
       {{-- NAVBAR --}}
@@ -46,7 +50,7 @@
             </div>
         </nav>
         {{-- HEADER --}}
-        <header class="container">
+        <header class="container mb-2">
           <div class="row align-items-center flex-sm-column flex-md-row">
             <div class="col-md-6">
               <h1>Selamat Datang di </h1> <h1 class="text-blue-main">Code Quorum!</h1>
@@ -58,6 +62,238 @@
               <img class="hero-image" src="{{ url("assets/img/illustration_home.jpg") }}" alt="Ilustrasi home page">
             </div>
           </div>
-        </header>        
+        </header>
+        {{-- MAIN --}}
+        <main>
+          {{-- SECTION 1 --}}
+            <section class="page-section" id="services">
+              <div class="container p-5">
+                  {{-- <div class="text-center">
+                      <h2 class="section-heading text-uppercase">Services</h2>
+                      <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                  </div> --}}
+                  <div class="row text-center">
+                      <div class="col-md-4">
+                          <span class="fa-stack fa-4x">
+                              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                              <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                          </span>
+                          <h4 class="my-3">Ask the Community</h4>
+                          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                      </div>
+                      <div class="col-md-4">
+                          <span class="fa-stack fa-4x">
+                              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                              <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                          </span>
+                          <h4 class="my-3">Responsive Design</h4>
+                          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                      </div>
+                      <div class="col-md-4">
+                          <span class="fa-stack fa-4x">
+                              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                              <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+                          </span>
+                          <h4 class="my-3">Web Security</h4>
+                          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                      </div>
+                  </div>
+                </div>
+            </section>
+          {{-- SECTION 2 --}}
+          <section class="page-section mt-2" id="services">
+            <div class="container p-5">
+              <div class="text-center mb-5">
+                <h2 class="section-heading">Tanya Jawab & Berkolaborasi</h2>
+                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+              </div>
+              {{-- Baris 1 --}}
+              <div class="row">
+                <!-- Kolom Pertama -->
+                <div class="col-md-4">
+                  <div class="card mb-4 border-0 shadow-lg rounded">
+                    <div class="row">
+                      <!-- Kolom Pertama di Baris Pertama -->
+                      <div class="col-md-2">
+                        <img src="{{ url("assets/img/user.png") }}" class="card-img-top img-small" alt="Gambar Postingan 1">
+                      </div>
+                      <!-- Kolom Kedua di Baris Pertama -->
+                      <div class="col-md-10">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Nama Pengguna</h5>
+                          <p class="card-text"><small class="text-muted">1 jam yang lalu</small></p>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Baris Kedua -->
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Kode JavaScript Error</h5>
+                          <p class="card-text">Permisi mau tanya, ada yang bisa bantu kenapa kode webpack saya di JavaScript tidak terbaca? ini sudah saya lampirkan screenshoot-nya juga. Terimakasih.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {{-- Kolom 2 --}}
+                <!-- Kolom Pertama -->
+                <div class="col-md-4">
+                  <div class="card mb-4 border-0 shadow-lg rounded">
+                    <div class="row">
+                      <!-- Kolom Pertama di Baris Pertama -->
+                      <div class="col-md-2">
+                        <img src="{{ url("assets/img/user.png") }}" class="card-img-top img-small" alt="Gambar Postingan 1">
+                      </div>
+                      <!-- Kolom Kedua di Baris Pertama -->
+                      <div class="col-md-10">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Nama Pengguna</h5>
+                          <p class="card-text"><small class="text-muted">1 jam yang lalu</small></p>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Baris Kedua -->
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Kode JavaScript Error</h5>
+                          <p class="card-text">Permisi mau tanya, ada yang bisa bantu kenapa kode webpack saya di JavaScript tidak terbaca? ini sudah saya lampirkan screenshoot-nya juga. Terimakasih.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {{-- Kolom 3 --}}
+                <!-- Kolom Pertama -->
+                <div class="col-md-4">
+                  <div class="card mb-4 border-0 shadow-lg rounded">
+                    <div class="row">
+                      <!-- Kolom Pertama di Baris Pertama -->
+                      <div class="col-md-2">
+                        <img src="{{ url("assets/img/user.png") }}" class="card-img-top img-small" alt="Gambar Postingan 1">
+                      </div>
+                      <!-- Kolom Kedua di Baris Pertama -->
+                      <div class="col-md-10">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Nama Pengguna</h5>
+                          <p class="card-text"><small class="text-muted">1 jam yang lalu</small></p>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Baris Kedua -->
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Kode JavaScript Error</h5>
+                          <p class="card-text">Permisi mau tanya, ada yang bisa bantu kenapa kode webpack saya di JavaScript tidak terbaca? ini sudah saya lampirkan screenshoot-nya juga. Terimakasih.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {{-- Baris 2 --}}
+              <div class="row">
+                <!-- Kolom Pertama -->
+                <div class="col-md-4">
+                  <div class="card mb-4 border-0 shadow-lg rounded">
+                    <div class="row">
+                      <!-- Kolom Pertama di Baris Pertama -->
+                      <div class="col-md-2">
+                        <img src="{{ url("assets/img/user.png") }}" class="card-img-top img-small" alt="Gambar Postingan 1">
+                      </div>
+                      <!-- Kolom Kedua di Baris Pertama -->
+                      <div class="col-md-10">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Nama Pengguna</h5>
+                          <p class="card-text"><small class="text-muted">1 jam yang lalu</small></p>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Baris Kedua -->
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Kode JavaScript Error</h5>
+                          <p class="card-text">Permisi mau tanya, ada yang bisa bantu kenapa kode webpack saya di JavaScript tidak terbaca? ini sudah saya lampirkan screenshoot-nya juga. Terimakasih.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {{-- Kolom 2 --}}
+                <!-- Kolom Pertama -->
+                <div class="col-md-4">
+                  <div class="card mb-4 border-0 shadow-lg rounded">
+                    <div class="row">
+                      <!-- Kolom Pertama di Baris Pertama -->
+                      <div class="col-md-2">
+                        <img src="{{ url("assets/img/user.png") }}" class="card-img-top img-small" alt="Gambar Postingan 1">
+                      </div>
+                      <!-- Kolom Kedua di Baris Pertama -->
+                      <div class="col-md-10">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Nama Pengguna</h5>
+                          <p class="card-text"><small class="text-muted">1 jam yang lalu</small></p>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Baris Kedua -->
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Kode JavaScript Error</h5>
+                          <p class="card-text">Permisi mau tanya, ada yang bisa bantu kenapa kode webpack saya di JavaScript tidak terbaca? ini sudah saya lampirkan screenshoot-nya juga. Terimakasih.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {{-- Kolom 3 --}}
+                <!-- Kolom Pertama -->
+                <div class="col-md-4">
+                  <div class="card mb-4 border-0 shadow-lg rounded">
+                    <div class="row">
+                      <!-- Kolom Pertama di Baris Pertama -->
+                      <div class="col-md-2">
+                        <img src="{{ url("assets/img/user.png") }}" class="card-img-top img-small" alt="Gambar Postingan 1">
+                      </div>
+                      <!-- Kolom Kedua di Baris Pertama -->
+                      <div class="col-md-10">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Nama Pengguna</h5>
+                          <p class="card-text"><small class="text-muted">1 jam yang lalu</small></p>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Baris Kedua -->
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="card-body bg-white">
+                          <h5 class="card-title">Kode JavaScript Error</h5>
+                          <p class="card-text">Permisi mau tanya, ada yang bisa bantu kenapa kode webpack saya di JavaScript tidak terbaca? ini sudah saya lampirkan screenshoot-nya juga. Terimakasih.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>                
+        </main>
+        {{-- FOOTER --}}
+        <footer>
+          <div class="bg-blue-main">
+            Ka
+          </div>
+        </footer>
+        {{-- SCRIPT --}}
+        <script>
+          window.addEventListener("scroll", function() {
+            var navbar = document.querySelector(".navbar");
+            navbar.classList.toggle("fixed-top", window.scrollY > 0);
+          });
+        </script>
     </body>
 </html>
